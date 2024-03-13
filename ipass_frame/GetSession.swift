4390394d1155e7f9728493cd649dfc5ecaac5dc4
mobileexpert1 <appsdev096@gmail.com>
2024-03-13 16:25:27 +0530

    Commit

---
+++ ipass_frame/GetSession.swift
@@ -0,0 +1,8 @@
+//
+//  GetSession.swift
+//  ipass_frame
+//
+//  Created by MOBILE on 13/03/24.
+//
+
+import Foundation
---
+++ ipass_frame/ipass_frame.h
@@ -0,0 +1,18 @@
+//
+//  ipass_frame.h
+//  ipass_frame
+//
+//  Created by MOBILE on 13/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ipass_frame.
+FOUNDATION_EXPORT double ipass_frameVersionNumber;
+
+//! Project version string for ipass_frame.
+FOUNDATION_EXPORT const unsigned char ipass_frameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ipass_frame/PublicHeader.h>
+
+
